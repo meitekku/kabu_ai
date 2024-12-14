69a10b4c10193598821f4ed096b3795a83b7bf9c
meitekku <meiteko@outlook.com>
2024-12-14 12:34:19 +0900

    change header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,11 +2,13 @@ import CompanySearch from "@/components/parts/common/CompanySearch";
 import { headers } from "next/headers";
 import Link from "next/link";
 
-const Header = async () => {
+async function getHeaderData() {
   const headersList = await headers();
   const pathname = headersList.get("x-invoke-path") || "";
-  const isRoot = pathname === "/";
-  
+  return { pathname };
+}
+
+const HeaderContent = ({ isRoot }: { isRoot: boolean }) => {
   const commonClasses = "logo justify-self-start pl-4 text-center w-full text-2xl";
   const logoLink = <Link href="/" className="hover:opacity-80">株AI</Link>;
 
@@ -22,7 +24,13 @@ const Header = async () => {
         <div className="iiarea justify-self-end pr-4"></div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+const Header = async () => {
+  const { pathname } = await getHeaderData();
+  const isRoot = pathname === "/";
+  return <HeaderContent isRoot={isRoot} />;
+};
+
+export default Header;
