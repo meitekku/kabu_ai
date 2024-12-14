8d602281ffd4e74f57789e791d9cad7a0162c2c0
meitekku <meiteko@outlook.com>
2024-12-14 10:18:17 +0900

    change conf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'http://kabu-ai.jp/',
+            },
+          ],
+          destination: '/:path*',
+        },
+      ],
+    }
+  },
+}
