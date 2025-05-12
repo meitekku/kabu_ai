165c3e7dff73e377306a67928a079805f80832d2
meitekku <meiteko@outlook.com>
2025-05-13 00:36:37 +0900

    change config

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import CompanySearch from "@/components/parts/common/CompanySearch";
+import { CurrentPriceInfo } from "@/components/common/CurrentPriceInfo";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from 'next/image';
@@ -27,7 +28,6 @@ const HeaderContent = ({ isRoot }: { isRoot: boolean }) => {
         </div>
         <div className="iiarea pr-4"></div>
       </div>
-      {/* 一時的にコメントアウト
       <div className="md:flex md:justify-center">
         <div className="overflow-x-auto">
           <div className="flex items-center space-x-4 whitespace-nowrap min-w-min">
@@ -40,7 +40,6 @@ const HeaderContent = ({ isRoot }: { isRoot: boolean }) => {
           </div>
         </div>
       </div>
-      */}
     </header>
   );
  };
