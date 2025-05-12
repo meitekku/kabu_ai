90432155909a4478fa679a22b7866208d75b3445
meitekku <meiteko@outlook.com>
2025-05-12 22:51:19 +0900

    change connfig

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import { NextConfig } from 'next'
+import { IncomingMessage, ServerResponse } from 'http'
 
 const nextConfig: NextConfig = {
   env: {
@@ -16,6 +17,14 @@ const nextConfig: NextConfig = {
   },
   async rewrites() {
     return [
+      {
+        source: '/_next/static/:path*',
+        destination: 'http://133.130.102.77:3000/_next/static/:path*',
+      },
+      {
+        source: '/static/:path*',
+        destination: 'http://133.130.102.77:3000/static/:path*',
+      },
       {
         source: '/:path*',
         destination: 'http://133.130.102.77:3000/:path*',
@@ -44,6 +53,22 @@ const nextConfig: NextConfig = {
   },
   httpAgentOptions: {
     keepAlive: true,
+  },
+  proxy: {
+    '/api': {
+      target: 'http://133.130.102.77:3000',
+      changeOrigin: true,
+      secure: false,
+      timeout: 30000,
+      proxyTimeout: 30000,
+      onError: (err: Error, req: IncomingMessage, res: ServerResponse) => {
+        console.error('Proxy Error:', err);
+        res.writeHead(500, {
+          'Content-Type': 'text/plain',
+        });
+        res.end('Proxy Error');
+      },
+    },
   },
 }
 
