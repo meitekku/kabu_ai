168313e3673f4e98ee4bdd938f6604dcf1770606
meitekku <meiteko@outlook.com>
2025-05-24 07:37:52 +0900

    revert

--- next.config.js
+++ next.config.js
@@ -47,19 +47,6 @@ const nextConfig = {
       {
         source: '/uploads/:path*',
         destination: '/var/www/kabu_ai/public/uploads/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'x-debug-path',
-            value: '(?<path>.*)',
-          },
-        ],
-        beforeFiles: [
-          {
-            source: '/uploads/:path*',
-            destination: '/api/debug-path?path=:path*',
-          },
-        ],
       },
     ];
   },
